fe5bb420931426d9ac8e06c0ad9ae80f5d197d89
Matthew T. Pham (Github.com) <mtpham.github@quantamail.net>
2025-07-03 00:52:35 -0500

    simplified nix shell

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,8 +1,10 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
-
-pkgs.mkShell.override { stdenv = pkgs.llvmPackages.libcxxStdenv; } {
+let
+  llvm-pkgs-ver = "19";
+in
+pkgs.mkShell.override { stdenv = pkgs."llvmPackages_${llvm-pkgs-ver}".libcxxStdenv; } {
   # There is an open issue regarding building libc++ std module
   # with `FORTIFY_SOURCE` flag which is enabled by default
   #
@@ -13,8 +15,10 @@ pkgs.mkShell.override { stdenv = pkgs.llvmPackages.libcxxStdenv; } {
   hardeningDisable = [ "fortify" ];
 
   nativeBuildInputs = [
-    (pkgs.llvmPackages.clang-tools.override { enableLibcxx = true; })
-    (pkgs.callPackage ./cmake-importstd-patched/package.nix { })
+    (pkgs."llvmPackages_${llvm-pkgs-ver}".clang-tools.override { enableLibcxx = true; })
+    pkgs."llvmPackages_${llvm-pkgs-ver}".libcxxClang
+
+    pkgs.cmake
     pkgs.ninja
     pkgs.ccache
 
@@ -27,7 +31,11 @@ pkgs.mkShell.override { stdenv = pkgs.llvmPackages.libcxxStdenv; } {
   ];
 
   shellHook = ''
+    # workaround required for cmake to find clang manifest (allowing import std)
+    # see https://github.com/NixOS/nixpkgs/issues/370217#issuecomment-2660926816
+    NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -B${pkgs."llvmPackages_${llvm-pkgs-ver}".libcxxClang.libcxx}/lib";
+
     # pre-commit install
-    ${pkgs.pre-commit}/bin/pre-commit install
+    ${pkgs.pre-commit}/bin/pre-commit install --allow-missing-config
   '';
 }
